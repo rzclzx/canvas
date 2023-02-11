879af416f60034e621dacdc4de67a9ad91620f81
zhangfei <1520175722@qq.com>
2023-02-11 17:17:33 +0800

    feat：泳道图

--- src/pages/g6/index.vue
+++ src/pages/g6/index.vue
@@ -2,7 +2,7 @@
   <div class="graph" @click="allInit">
     <div class="header"></div>
     <div class="flex-between-start" style="position:relative">
-      <div class="scroll">
+      <!-- <div class="scroll">
         <el-slider
           v-model="size"
           vertical
@@ -13,7 +13,7 @@
           @input="scale"
         >
         </el-slider>
-      </div>
+      </div> -->
       <div class="panel" id="mountNode"></div>
       <div>
         <el-card>
@@ -45,6 +45,7 @@
 <script>
 import G6 from '@antv/g6';
 import options from './options';
+import './style.css'
 export default {
   components: {},
   data () {
@@ -54,7 +55,13 @@ export default {
       data: {},
       graph: {},
       tableData: [],
-      state: false
+      state: false,
+      // 节点宽度
+      nodeWidth: 100,
+      // 节点高度
+      nodeHeight: 50,
+      // 泳道宽度
+      pipWidth: 200
     }
   },
   mounted () {
@@ -63,7 +70,7 @@ export default {
   methods: {
     scale () {
       if (this.graph.zoomTo) {
-        this.graph.zoomTo(this.size, this.getCenter())
+        // this.graph.zoomTo(this.size, this.getCenter())
       }
     },
     getCenter () {
@@ -80,28 +87,166 @@ export default {
       }
     },
     async init () {
-      const res = await fetch(
-        'https://gw.alipayobjects.com/os/basement_prod/6cae02ab-4c29-44b2-b1fd-4005688febcb.json'
-      );
-      this.data = await res.json();
+      // const res = await fetch(
+      //   'https://gw.alipayobjects.com/os/basement_prod/6cae02ab-4c29-44b2-b1fd-4005688febcb.json'
+      // );
+      // this.data = await res.json();
+      G6.registerNode(
+        'dom-pip',
+        {
+          draw: (cfg, group) => {
+            const keyShape = group.addShape('rect', {
+              attrs: {
+                width: cfg.size[0],
+                height: cfg.size[1],
+                fill: `${cfg.index%2 === 0 ? '#bebebe' : '#fff'}`,
+                opacity: .3
+              },
+              name: 'dom-pip',
+              capture: false
+            })
+            group.addShape('rect', {
+              attrs: {
+                width: this.pipWidth,
+                height: 30,
+                fill: `#606266`,
+                y: 0,
+                x: 0
+              },
+              name: 'dom-pip-header',
+            })
+            group.addShape('text', {
+              attrs: {
+                text: cfg.label,
+                fill: '#fff',
+                y: 20,
+                x: 0
+              },
+              name: 'dom-pip-header',
+            })
+            return keyShape
+          },
+        }
+      )
+      G6.registerNode(
+        'dom-node',
+        {
+          draw: (cfg, group) => {
+            const keyShape = group.addShape('rect', {
+              attrs: {
+                width: cfg.size[0],
+                height: cfg.size[1],
+                fill: '#409EFF'
+              },
+              name: 'dom-node',
+            })
+            group.addShape('text', {
+              attrs: {
+                text: cfg.label,
+                fill: '#fff',
+                y: 30,
+                x: 20,
+              },
+              name: 'dom-pip-header',
+            })
+            return keyShape
+          },
+        }
+      )
       this.graph = new G6.Graph(this.options);
       this.dataInit();
-      console.log(this.data)
       this.graph.data(this.data);
       this.graph.render();
       this.addEvent();
     },
     dataInit () {
-      this.data.nodes.forEach(item =>{
-        if (item.id == 0) [
-          item.size = 50
-        ]
-      })
-      this.data.edges.forEach(item => {
-        if (item.label == 'e0-4') {
-          item.color = '#F07676'
+      this.data = { nodes: [], edges: [] }
+      for (let i = 1; i <= 5; i++) {
+        this.data.nodes.push({
+          id: i + '',
+          type: 'dom-pip',
+          label: i + '',
+          index: i,
+          x: i * this.pipWidth - this.pipWidth,
+          y: 0,
+          size: [this.pipWidth, 3000]
+        })
+      }
+      for (let i = 1; i <= 5; i++) {
+        for (let j = 1; j <= Math.pow(2, i - 1); j++) {
+          if (i === 5) {
+            break
+          }
+          this.data.nodes.push({
+            id: `${i}-${j}`,
+            type: 'dom-node',
+            label: `node${i}-${j}`,
+            anchorPoints: [
+              [0, 0.5], // 左侧中间
+              [1, 0.5], // 右侧中间
+            ],
+            x: (i === 5 ? 6 : i)*this.pipWidth - this.pipWidth + 40,
+            size: [this.nodeWidth, this.nodeHeight]
+          })
+          if (i <= 3) {
+            for (let q = 1; q <= 2; q++) {
+              this.data.edges.push({
+                type: 'polyline',
+                source: `${i}-${j}`,
+                target: `${i + 1}-${2*j - (2 - q)}`
+              })
+            }
+          }
         }
+      }
+      const node = this.data.nodes.find(item => item.id === '1-1')
+      this.setYpos(this.data, node, { index: 0 })
+      // mock 结果列
+      let list = this.data.nodes.filter(item => item.label.indexOf('node4') >= 0)
+      list.forEach((item, index) => {
+        this.data.nodes.push({
+          id: `${5}-${index + 1}`,
+          type: 'dom-node',
+          label: `${5}-${index + 1}`,
+          anchorPoints: [
+            [0, 0.5], // 左侧中间
+            [1, 0.5], // 右侧中间
+          ],
+          x: 5*this.pipWidth - this.pipWidth + 40,
+          y: item.y,
+          size: [this.nodeWidth, this.nodeHeight]
+        })
+        this.data.edges.push({
+          type: 'polyline',
+          source: item.id,
+          target: `${5}-${index + 1}`
+        })
       })
+      console.log(this.data)
+    },
+    setYpos(data, parent, pos) {
+      const children = data.edges.filter(edge => edge.source === parent.id)
+      if (children.length) {
+        let h, start, end
+        children.forEach((child, index) => {
+          const node = data.nodes.find(node => node.id === child.target)
+          let val = this.setYpos(data, node, pos)
+          if (index === 0) {
+            start = val
+          }
+          if (index === children.length - 1) {
+            end = val
+          }
+        })
+        h = (start + end + this.nodeHeight)/2 - this.nodeHeight/2
+        parent.y = h
+        return h
+      } else {
+        let h = pos.index * (this.nodeHeight*2) + 50
+        pos.index++
+        parent.y = h
+        return h
+      }
     },
     addEvent () {
       this.addHover();
@@ -197,15 +342,18 @@ export default {
   .graph {
     width: 100%;
     height: 1080px;
-    .header {
-      height: 80px;
-      background: #7CC8FB;
-    }
   }
   .scroll {
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
     left: 50px;
+  }
+  .panel {
+    border: solid 1px #bebebe;
+  }
+  .pip {
+    height: 600px;
+    width: 100px;
   }
 </style>
--- src/pages/g6/options.js
+++ src/pages/g6/options.js
@@ -1,51 +1,8 @@
 export default {
   container: 'mountNode', // String | HTMLElement，必须，在 Step 1 中创建的容器 id 或容器本身
-  width: 1600, // Number，必须，图的宽度
-  height: 900, // Number，必须，图的高度
-  layout: {                // Object，可选，布局的方法及其配置项，默认为 random 布局。
-    type: 'radial',         // 指定为力导向布局
-    preventOverlap: true,  // 防止节点重叠
-    nodeSize: 100,      // 节点大小，用于算法中防止节点重叠时的碰撞检测。由于已经在上一节的元素配置中设置了每个节点的 size 属性，则不需要在此设置 nodeSize。
-    linkDistance: 100, // 指定边距离为100
-  },
+  width: 1400, // Number，必须，图的宽度
+  height: 700, // Number，必须，图的高度
   modes: {
-    default: ['drag-canvas', 'drag-node', 'activate-relations'], // 允许拖拽画布、拖拽节点
-  },
-  defaultNode: {
-    size: 30,
-    style: {
-      lineWidth: 2,
-      stroke: '#6BC1FA',
-      fill: '#C6E5FF',
-    },
-  },
-  defaultEdge: {
-    size: 3,
-    color: '#e2e2e2',
-    style: {
-      endArrow: {
-        path: 'M 0,0 L 8,4 L 8,-4 Z',
-        fill: '#e2e2e2'
-      },
-    },
-  },
-  nodeStateStyles: {
-    // 鼠标 hover 上节点，即 hover 状态为 true 时的样式
-    hover: {
-      fill: '#7CC8FB',
-      stroke: '#7CC8FB'
-    },
-    // 鼠标点击节点，即 click 状态为 true 时的样式
-    click: {
-      fill: '#7CC8FB',
-      stroke: '#7CC8FB'
-    }
-  },
-  // 边不同状态下的样式集合
-  edgeStateStyles: {
-    // 鼠标点击边，即 click 状态为 true 时的样式
-    click: {
-      stroke: '#7CC8FB'
-    },
-  },
+    default: ['zoom-canvas', 'drag-canvas'], // 允许拖拽画布、拖拽节点
+  }
 }
